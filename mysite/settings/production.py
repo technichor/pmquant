10765097a31f11ba44558f36efbc8ba91a3711f2
corey-desktop <corey.b.becker@gmail.com>
2020-04-05 12:06:37 -0400

    getting settings ready for production deploy

---
+++ mysite/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+
+ALLOWED_HOSTS = ['technichor.pythonanywhere.com']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'PORT': config('DB_HOST'),
+    }
+}
